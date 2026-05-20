f97d16d3f799062c7a39e1db9b58b0a05046e4e9
Ahmed Raza <asmart279@gmail.com>
2026-05-20 20:26:59 +0500

    refactor: updated App Group to group.com.hanotech.justdrink.apppgroup to match user's registered ID

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -5,7 +5,7 @@ class AppConstants {
   static const interstitialAdUnitId = 'ca-app-pub-3940256099942544/1033173712';
 
   // Widget identifiers
-  static const widgetAppGroupId = 'group.com.hanotech.justdrink.3MKRPJ4PJ2';
+  static const widgetAppGroupId = 'group.com.hanotech.justdrink.apppgroup';
   static const widgetAndroidName = 'com.justdrink.app.JustDrinkWidgetProvider';
   static const widgetIOSName = 'JustDrinkWidget';
 
